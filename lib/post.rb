b89ae68f3ac39b39c0307a36a8ea44d17d1bdc02
Scully87 <scully87.ts@googlemail.com>
2014-12-08 18:28:06 +0000

    name text indent

--- lib/post.rb
+++ lib/post.rb
@@ -6,8 +6,11 @@ class Post
   include DataMapper::Resource
 
   property :id,     Serial
-  property :name, String
-  property :body,  String
+  property :name, String, :required => true, :length => 3..20
+  property :body,  String, :required => true, :length => 5..500 
   property :created_at, DateTime
+
+  validates_length_of :name,  :message => "Please Enter Your Name"
+  validates_length_of :body,  :message => "Please Enter Your Comment"
 
 end
